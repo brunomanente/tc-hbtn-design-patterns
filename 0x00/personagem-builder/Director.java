b1cc9ab4c7d446cff1e9494ef6c7a630af874c4f
bmanente <bmanente@ciandt.com>
2022-05-02 15:44:32 -0300

    Factory Method de Personagens

--- 0x00/personagem-builder/Director.java
+++ 0x00/personagem-builder/Director.java
@@ -1,6 +1,6 @@
 public class Director {
 
-    void buildGuerreiro(Builder builder){
+    public void buildGuerreiro(Builder builder){
         builder.setNome("Guerreiro");
         builder.setTipo(TipoPersonagem.GUERREIRO);
         builder.setInteligencia(1);
@@ -10,7 +10,7 @@ public class Director {
         builder.setResistencia(10);
     }
 
-    void buildLadrao(Builder builder){
+    public void buildLadrao(Builder builder){
         builder.setNome("Ladrao");
         builder.setTipo(TipoPersonagem.LADRAO);
         builder.setInteligencia(2);
@@ -20,7 +20,7 @@ public class Director {
         builder.setResistencia(5);
     }
 
-    void buildMago(Builder builder){
+    public void buildMago(Builder builder){
         builder.setNome("Mago");
         builder.setTipo(TipoPersonagem.MAGO);
         builder.setInteligencia(10);

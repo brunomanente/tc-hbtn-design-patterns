0324a50a4fb87c840ef1ad0dc245e88af5004b1f
bmanente <bmanente@ciandt.com>
2022-05-02 15:46:19 -0300

    Factory Method de Personagens

--- 0x00/personagem-builder/Program.java
+++ 0x00/personagem-builder/Program.java
@@ -1,6 +1,8 @@
 import java.util.Locale;
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(new Locale("pt", "BR"));
+
         Director director = new Director();
 
         GuerreiroBuilder guerreiroBuilder = new GuerreiroBuilder();

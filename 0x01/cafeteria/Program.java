12dde473c02caa5661adb6402de72a60e29e87ef
bmanente <bmanente@ciandt.com>
2022-05-03 10:04:41 -0300

    Cafeteria

--- 0x01/cafeteria/Program.java
+++ 0x01/cafeteria/Program.java
@@ -1,5 +1,8 @@
+import java.util.Locale;
+
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(new Locale("pt", "BR"));
         Bebida cafe = new BebidaCafe();
         System.out.println(cafe);
 
